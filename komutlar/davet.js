62b61625222814a4a8127f5e67f7ea898b7366c8
KinqPlayerTR <42252885+KinqPlayerTR@users.noreply.github.com>
2018-11-05 19:29:32 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -7,7 +7,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('İşte Davet Linkim: https://bit.ly/Kraliyetbot');
+    .setDescription('İşte Davet Linkim: https://bit.ly/KraliyetBot');
     message.channel.sendEmbed(ozelmesajkontrol) }
 };
 
